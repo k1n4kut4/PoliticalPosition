f6bccbe1867c9bb5c999122536cd01418bd06d6d
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-08-18 04:52:32 +0100

    add questionResponse algorithm

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -18,13 +18,24 @@ export class QuestionComponent implements OnInit {
   public questions: Question[]; 
   public question: Observable<Question>;
   private questionsSubscription: Subscription;
+
   public current_question: number;  
+
+  public econ: number; 
+  public dipl: number; 
+  public govt: number; 
+  public scty: number; 
 
   constructor(private data: DataService) { }
 
   ngOnInit() {
 
     this.current_question = 1;
+
+    this.econ = 0;
+    this.dipl = 0;
+    this.govt = 0;
+    this.scty = 0;
 
     //subscribe to questions, from questions.json
     this.questionsSubscription = this.data.getQuestions().subscribe(res => { 
@@ -43,6 +54,30 @@ export class QuestionComponent implements OnInit {
 
   }
 
+  questionResponse(mult: number){
+    this.econ += mult * this.questions[0].econ;
+    this.dipl += mult * this.questions[0].dipl;
+    this.govt += mult * this.questions[0].govt;
+    this.scty += mult * this.questions[0].scty; 
 
+    //update storage
+    
+    
+    this.current_question++; 
+
+    if (this.current_question < 70) {
+      //reroute
+      
+
+    } else {
+      this.results();
+    }
+  }
+
+  results(){
+
+    //reroute
+
+  }
 
 }
