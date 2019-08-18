8be2b8d4f91ea5255fa7d68d06dcdb919d6b74b0
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-08-18 03:36:51 +0100

    add AppRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { QuestionComponent } from './components/question/question.component';
+import { ResultsComponent } from './components/results/results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'question/:id', component: QuestionComponent },
+  { path: 'results/:a/:b/:c/:d', component: ResultsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
